51d9357e7d5e4bfa62e236e1cc8846a77b5ea517
Francis WONG <fwong@e1r9p18.clusters.42paris.fr>
2022-05-15 14:03:04 +0200

    release

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,24 +6,29 @@
 /*   By: fwong <fwong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 19:57:45 by fwong             #+#    #+#             */
-/*   Updated: 2022/05/15 11:52:05 by fwong            ###   ########.fr       */
+/*   Updated: 2022/05/15 13:43:40 by fwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_count_char(char *str, char c)
+int	ft_count_char(char const*str, char c, int index)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] && str[i] != c)
+	while (str[i])
+	{
+		if (str[i] != c)
 			i++;
+		else
+			return (0);
+	}
 	return (i);
 }
 
-int	ft_count_words(char *str, char c)
+int	ft_count_words(char const*str, char c)
 {
 	int	i;
 	int	j;
@@ -41,26 +46,32 @@ int	ft_count_words(char *str, char c)
 	return (j);
 }
 
-char	*ft_malloc_and_fill(char *s, int c, int wordCount)
+char	*ft_malloc_and_fill(char const *s, char c, int wordCount)
 {
-	int	i;
+	int	index;
 	int	j;
+	int	k;
 	char	*str;
 	size_t	charCount;
 
-	i = 0;
+	index = 0;
 	j = 0;
 	while (wordCount--)
 	{
 		while (s[j] == c)
-			j++;
-		charCount = ft_count_char(s + j, c);
-		str[i] = malloc(sizeof(char) * charCount + 1);
-		printf("%d\n", j);
-
-		j = charCount + j;
-		printf("%d\n", j);
-		i++;
+			index++;
+		charCount = ft_count_char(s, c, index);
+		str = malloc(sizeof(char) * charCount + 1);
+		if (!str)
+			return (NULL);			
+		printf("%ld\n", charCount);
+		k = charCount + j;
+		while (index < k)
+		{
+			str[index++] = s[j++];
+		}
+		index = 0;
+		index++;
 	}
 }
 
@@ -69,26 +80,14 @@ char	**ft_split(char const *s, char c)
 	int		i;
 	int		j;
 	size_t		wordCount;
-	size_t		charCount;
 	char	**tab;
 	
 	wordCount = ft_count_words(s, c);
 	tab = malloc(sizeof(char *) * ft_count_words((char *)s, c));
 	if (!tab)
 		return (NULL);
-	i = 0;
-	j = 0;
-	while (wordCount--)
-	{
-		while (s[j] == c)
-			j++;
-		charCount = ft_count_char(s + j, c);
-		tab[i] = (char) malloc(sizeof(char) * charCount);
-		i++;
-		while (s[j] != c)
-			j++;
-	}
-	tab = ft_fill(s, c);
+	printf("caca\n");
+	ft_malloc_and_fill(s, c, wordCount);
 	return (tab);
 }
 
@@ -96,8 +95,8 @@ int main()
 {
 	char a[] = "Bonjour je nnn  ds  ds";
 
-	// ft_split(a, '/');
+	// ft_split(a, '/');ls
 	
 	// printf("%d", ft_count_separator(a, ' '));
-	printf("%s", a + 2);
+	ft_split(a, ' ');
 }
