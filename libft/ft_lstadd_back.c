cecb87aad2ee774d9ca0702d44bd8bc76315399a
Francis WONG <fwong@e2r3p18.clusters.42paris.fr>
2022-05-27 17:54:37 +0200

    release

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: fwong <fwong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 04:35:29 by fwong             #+#    #+#             */
-/*   Updated: 2022/05/27 04:46:17 by fwong            ###   ########.fr       */
+/*   Updated: 2022/05/27 17:53:26 by fwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,27 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*save_lst;
-
-	if (new && lst)
-	{
-		if (!*lst)
-		{
-			*lst = new;
-			return ;
-		}
-		save_lst = ft_lstlast(*lst);
-		save_lst->next = new;
-	}
+	if (*lst == NULL)
+		*lst = new;
+	else
+	ft_lstlast(*lst)->next = new;
 }
+/* 
+#include <stdio.h>
+int main (int ac, char **av)
+{
+    (void)ac;
+    t_list *elem;
+    int i = 2;
+    elem = ft_lstnew((void *)av[1]);
+    while (av[i])
+    {
+        ft_lstadd_back(&elem, ft_lstnew((void *)av[i]));
+        i++;
+    }
+    while (elem)
+    {
+        printf("%s\n", (char *)elem->content);
+        elem = elem->next;
+    }
+} */
--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: fwong <fwong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 04:31:23 by fwong             #+#    #+#             */
-/*   Updated: 2022/05/27 04:41:08 by fwong            ###   ########.fr       */
+/*   Updated: 2022/05/27 17:19:48 by fwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,6 @@ t_list	*ft_lstlast(t_list *lst)
 	if (!lst)
 		return (0);
 	while (lst->next)
-	{
-		if (lst->next == NULL)
-			return lst;
-	}
+		lst = lst->next;
 	return (lst);
 }
-
-/* if (!lst)
-		return (0);
-	while (lst->next)
-		lst = lst->next;
-	return (lst); */
